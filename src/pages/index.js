65184cc0cc28acdc63e0f8ed71a266604bf08859
Csaba Farkas <csabafarkas17@gmail.com>
2019-06-20 21:54:59 +0100

    Added hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,32 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { FaGulp } from "react-icons/fa"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { FaGulp } from 'react-icons/fa'
+import BackgroundSection from '../components/Globals/BackgroundSection'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
+const IndexPage = ({ data }) => (
+    <Layout>
+        <SEO title="Home" />
+        <BackgroundSection
+            img={data.img.childImageSharp.fluid}
+            title="regular joe's"
+            styleClass="default-background"
+        />
+    </Layout>
 )
+
+export const query = graphql`
+    {
+        img: file(relativePath: { eq: "default-background.jpeg" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid_tracedSVG
+                }
+            }
+        }
+    }
+`
 
 export default IndexPage
